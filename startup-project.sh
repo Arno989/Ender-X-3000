5671b9cac39f3711abff4964bbe096000ba0f7d8
Arno Defauw <arno@delandtsheer.eu>
2019-06-11 12:58:00 +0200

    skeet

---
+++ startup-project.sh
@@ -0,0 +1,11 @@
+#!/bin/bash  
+
+tmux new-session -d -s dataserial 'python3 /home/arno/Ender-X-3000/Datacom/SerialCommunication.py'
+tmux new-session -d -s dataapp 'python3 /home/arno/Ender-X-3000/Datacom/App.py'
+tmux new-session -d -s backend 'python3 /home/arno/Ender-X-3000/www/backend/app.py'
+
+trap "tmux kill-session -t dataserial; tmux kill-session -t dataapp; tmux kill-session -t backend; exit" HUP INT TERM
+
+while :; do sleep 5; done;
+
+exit 0
